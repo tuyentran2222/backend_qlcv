976ddf03d0b9957d5da3297f8339b4e086a3620b
tuyentran2222 <tranvantuyen02082000@gmail.com>
2022-01-28 17:52:06 +0700

    add column role to members table

---
+++ database/migrations/2022_01_28_105030_add_role_column_to_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleColumnToMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            $table->integer('role')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+        });
+    }
+}
